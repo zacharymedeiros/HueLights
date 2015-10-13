e4a87b7b9849d199a731462afc0a2cf66584d011
zacharymedeiros <z.medeiros.rodriguez@gmail.com>
2015-10-12 19:18:01 -0700

    Activate Personal Controls
    added functionality

--- login.js
+++ login.js
@@ -1,20 +1,16 @@
 FB.api('/me', function(response) {
-    console.log(JSON.stringify(response));
-});
-
-
-FB.api('/me', function(response) {
     window.username=(response);
 });
 
+window.setup = function() {
 if (username = "Zach Medeiros") {
-	$("h5").hide();
+	$("h5").show();
 }
 else if (username = "Waseem Ahmad") {
-	$("h6").hide();
+	$("h6").show();
 }
 else if (username = "Prashanth Bala") {
-	$("h7").hide();
+	$("h7").show();
+}
 }
 
-}
