0505652a1aaa823811a8de8422322450eada626e
zacharymedeiros <z.medeiros.rodriguez@gmail.com>
2015-10-16 16:36:02 -0700

    Fixed selectLight.js
    IP’s changed after bridge software update

--- selectLight.js
+++ selectLight.js
@@ -1,4 +1,3 @@
-
 "use strict";
 window.destination;
 
@@ -15,24 +14,25 @@ class HueLight {
 
 //Fill lightList with list of lights and their properties
 var lightList = [
-	new HueLight("Zach", "1", "10.0.1.2", "1a40099c2d5d42af1feb3b7430811e27", "top"),
-	new HueLight("Zach", "2", "10.0.1.2", "1a40099c2d5d42af1feb3b7430811e27", "bottom"),
-	new HueLight("Zach", "3", "10.0.1.2", "1a40099c2d5d42af1feb3b7430811e27", "middle"),
-	new HueLight("Zach", "4", "10.0.1.2", "1a40099c2d5d42af1feb3b7430811e27", "desk"),
-	new HueLight("Waseem", "1", "10.0.1.3", "228d53983981807f2f762d481558508f", "bottom"),
-	new HueLight("Waseem", "2", "10.0.1.3", "228d53983981807f2f762d481558508f", "top"),
-	new HueLight("Waseem", "3", "10.0.1.3", "228d53983981807f2f762d481558508f", "nightstand"),
-	new HueLight("Waseem", "4", "10.0.1.3", "228d53983981807f2f762d481558508f", "desk"),
-	new HueLight("Bala", "11", "10.0.1.3", "228d53983981807f2f762d481558508f", "nighstand"),
-	new HueLight("Bala", "12", "10.0.1.3", "228d53983981807f2f762d481558508f", "bottom"),
-	new HueLight("Bala", "13", "10.0.1.3", "228d53983981807f2f762d481558508f", "top"),
-	new HueLight("Bala", "14", "10.0.1.3", "228d53983981807f2f762d481558508f", "dresser"),
+	new HueLight("Zach", "1", "10.0.1.15", "36c82706294f104bc187523c0761ff", "top"),
+	new HueLight("Zach", "2", "10.0.1.15", "36c82706294f104bc187523c0761ff", "bottom"),
+	new HueLight("Zach", "3", "10.0.1.15", "36c82706294f104bc187523c0761ff", "middle"),
+	new HueLight("Zach", "4", "10.0.1.15", "36c82706294f104bc187523c0761ff", "desk"),
+	new HueLight("Waseem", "1", "10.0.1.7", "3a40927e39be4dc71949fe921a00f1eb", "bottom"),
+	new HueLight("Waseem", "2", "10.0.1.7", "3a40927e39be4dc71949fe921a00f1eb", "top"),
+	new HueLight("Waseem", "3", "10.0.1.7", "3a40927e39be4dc71949fe921a00f1eb", "nightstand"),
+	new HueLight("Waseem", "4", "10.0.1.7", "3a40927e39be4dc71949fe921a00f1eb", "desk"),
+	new HueLight("Bala", "11", "10.0.1.7", "3a40927e39be4dc71949fe921a00f1eb", "nighstand"),
+	new HueLight("Bala", "12", "10.0.1.7", "3a40927e39be4dc71949fe921a00f1eb", "bottom"),
+	new HueLight("Bala", "13", "10.0.1.7", "3a40927e39be4dc71949fe921a00f1eb", "top"),
+	new HueLight("Bala", "14", "10.0.1.7", "3a40927e39be4dc71949fe921a00f1eb", "dresser"),
 	new HueLight("LivingRoom", "23", "10.0.1.9", "2de1effa3924cd37296e496630b80653", "kitchen"),
 	new HueLight("LivingRoom", "24", "10.0.1.9", "2de1effa3924cd37296e496630b80653", "top"),
 	new HueLight("LivingRoom", "25", "10.0.1.9", "2de1effa3924cd37296e496630b80653", "tv"),
 	new HueLight("LivingRoom", "26", "10.0.1.9", "2de1effa3924cd37296e496630b80653", "bottom"),
 	new HueLight("LivingRoom", "27", "10.0.1.9", "2de1effa3924cd37296e496630b80653", "bar")
 ];
+
 
 function selectLight(lightID) {
 	//Initialize destination URL based on light selection
